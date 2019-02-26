82d76767005df721e940391acf55d0bbb75060f4
LaughingLeader <laughingleader@gmail.com>
2019-02-26 05:27:39 -0600

    Added an initial export helper, added an initial update to the 3d ui layer manager, added a flag helper for dos2de

--- UV_Helpers.py
+++ UV_Helpers.py
@@ -1,6 +1,9 @@
+from __future__ import division
 import bpy
 import bmesh
 import os
+
+import math
 
 from bpy.types import Operator, PropertyGroup
 from bpy.props import CollectionProperty, PointerProperty
@@ -26,24 +29,30 @@ class UVError:
         self.loop1 = loop1
         self.loop2 = loop2
 
+class UVErrorv2:
+    def __init__(self, face, vert1, vert2, vert3, uvloop1, uvloop2, uvloop3):
+        self.face = face
+        self.vert1 = vert1
+        self.vert2 = vert2
+        self.vert3 = vert3
+        self.loop1 = uvloop1
+        self.loop2 = uvloop2
+        self.loop3 = uvloop3
+
 class UVUnwrappedChecker(Operator):
     """Check for unwrapped UVs that will cause tangent/binormal issues.\nThese are UVs from separate vertices that share the exact same UV coordinates for a face."""      # Use this as a tooltip for menu items and buttons.
     bl_idname = "uvhelpers.unwrappedchecker"        # Unique identifier for buttons and menu items to reference.
     bl_label = "Check for Unwrapped UVs"         # Display name in the interface.
     bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
 
+    @classmethod
+    def poll(cls, context):
+        return (context.mode == 'EDIT_MESH')
+
     def uv_error(self, vert1, vert2, uv1, uv2):
-        if vert1 != vert2 and uv1 == uv2:
-            return True
+        return vert1 != vert2 and uv1 == uv2
 
-    def execute(self, context):        # execute() is called when running the operator.
-
-        # The original script
-        scene = context.scene
-        node = bpy.context.scene.objects.active
-
-        bpy.ops.object.mode_set(mode="EDIT")
-
+    def uv_checkforerrors(self, context, node):
         if node.type == "MESH":
             if (node.data is not None):
 
@@ -53,6 +62,7 @@ class UVUnwrappedChecker(Operator):
 
                 bm = bmesh.from_edit_mesh(mesh)
                 bm.select_mode = {"VERT"}
+                #bm.select_mode = {"VERT"}
                 bm.select_flush(False)
                 uv_layer = bm.loops.layers.uv.active
 
@@ -102,7 +112,109 @@ class UVUnwrappedChecker(Operator):
                     
                 bm.select_flush(True)
                 bmesh.update_edit_mesh(mesh)
+
+    def uv_error_v2(self, uv1, uv2, uv3):
+        s1 = float(uv2[0] - uv1[0]) # x
+        s2 = float(uv3[0] - uv1[0])
+        t1 = float(uv2[1] - uv1[1]) # y
+        t2 = float(uv3[1] - uv1[1])
+        
+        check1 = float(s1 * t2 - s2 * t1)
+
+        return abs(check1) < 0.00001
+
+    def uv_checkforerrors_v2(self, context, node):
+        if node.type == "MESH":
+            if (node.data is not None):
+
+                bpy.context.tool_settings.mesh_select_mode = (True, False, False)
+                #py.context.tool_settings.mesh_select_mode = (False, False, True)
+
+                mesh = node.data
+
+                bm = bmesh.from_edit_mesh(mesh)
+                bm.select_flush(False)
+                uv_layer = bm.loops.layers.uv.active
+
+                uv_errors = []
+
+                for face in bm.faces:
+                    if face in uv_errors:
+                        continue
+
+                    face.select = False
+                    
+                    if(len(face.loops) == 3):
+                        vloop1 = face.loops[0]
+                        vloop2 = face.loops[1]
+                        vloop3 = face.loops[2]
+
+                        vert1 = vloop1.vert
+                        vert2 = vloop2.vert
+                        vert3 = vloop3.vert
+
+                        vert1.select_set(False)
+                        vert2.select_set(False)
+                        vert3.select_set(False)
+                        
+                        uvloop1 = vloop1[uv_layer]
+                        uvloop2 = vloop2[uv_layer]
+                        uvloop3 = vloop3[uv_layer]
+
+                        uv1 = uvloop1.uv
+                        uv2 = uvloop2.uv
+                        uv3 = uvloop3.uv
+
+                        #uv1 = vert1.co.xy
+                        #uv2 = vert2.co.xy
+                        #uv3 = vert3.co.xy
+
+                        if self.uv_error_v2(uv1, uv2, uv3):
+                            uv_error_entry = UVErrorv2(face, vert1, vert2, vert3, uvloop1, uvloop2, uvloop3)
+                            uv_errors.append(uv_error_entry)
+                        
+                total_errors = len(uv_errors)
+                if total_errors > 0:
+                    for uv_error in uv_errors:
+                        print("[ERROR]: UV problem detected!")
+                        print("  Vert1: (%f,%f,%f)" % uv_error.vert1.co[:])
+                        print("  Vert2: (%f,%f,%f)" % uv_error.vert2.co[:])
+                        print("  Vert3: (%f,%f,%f)" % uv_error.vert3.co[:])
+                        print("  UV1: (%f,%f)" % uv_error.loop1.uv[:])
+                        print("  UV2: (%f,%f)" % uv_error.loop2.uv[:])
+                        print("  UV3: (%f,%f)" % uv_error.loop3.uv[:])
+                        uv_error.vert1.select_set(True)
+                        uv_error.vert2.select_set(True)
+                        uv_error.vert3.select_set(True)
+                        uv_error.loop1.select = True
+                        uv_error.loop2.select = True
+                        uv_error.loop3.select = True
+
+                    self.report({"WARNING"}, "[LL-UV-Helper] {} total problems found on UV map. Check selected vertices for wrapping issues.".format(total_errors))
+                else:
+                    self.report({"INFO"}, "[LL-UV-Helper] No UV problems found.")
+                
+                #total_errors.clear()
+
+                bm.select_flush(True)
+                bmesh.update_edit_mesh(mesh)
+
+    def execute(self, context):        # execute() is called when running the operator.
+
+        # The original script
+        scene = context.scene
+
+        if bpy.context.scene.objects.active is None:
+            self.report({"WARNING"}, "[LL-UV-Helper] Select a mesh before checking UVs!")
+            return {'FINISHED'}
+
+        node = bpy.context.scene.objects.active
+
+        bpy.ops.object.mode_set(mode="EDIT")
+        self.uv_checkforerrors_v2(context, node)
+
         return {'FINISHED'}            # Lets Blender know the operator finished successfully.
+
 
 class UVHelperPanel(bpy.types.Panel):
     bl_label = "Helpers"
@@ -111,18 +223,21 @@ class UVHelperPanel(bpy.types.Panel):
     bl_region_type = 'TOOLS'
     bl_category = 'Helpers'
 
+    @classmethod
+    def poll(cls, context):
+        return (context.object is not None)
+
     def draw(self, context):
         layout = self.layout
-
         layout.operator(UVUnwrappedChecker.bl_idname)
 
 def register():
-    #bpy.utils.register_class(UVError)
-    bpy.utils.register_class(UVUnwrappedChecker)
-    bpy.utils.register_class(UVHelperPanel)
-
+    #bpy.utils.register_module(__name__)
+    return
 
 def unregister():
-    #bpy.utils.unregister_class(UVError)
-    bpy.utils.unregister_class(UVUnwrappedChecker)
-    bpy.utils.unregister_class(UVHelperPanel)
+    #bpy.utils.unregister_module(__name__)
+    return
+
+if __name__ == "__main__":
+    register()
--- __init__.py
+++ __init__.py
@@ -1,6 +1,3 @@
-import bpy
-from . import UV_Helpers
-
 bl_info = {
     "name": "LaughingLeader Helpers",
     "author": "LaughingLeader",
@@ -14,13 +11,101 @@ bl_info = {
     "support": "COMMUNITY",
     "category": "System"}
 
+import bpy
+
+from bpy.types import (
+        Operator,
+        Panel,
+        UIList,
+        PropertyGroup,
+        AddonPreferences,
+        )
+from bpy.props import (
+        StringProperty,
+        BoolProperty,
+        IntProperty,
+        CollectionProperty,
+        BoolVectorProperty,
+        PointerProperty,
+        )
+
+# load and reload submodules
+##################################
+
+import importlib
+from . import developer_utils
+importlib.reload(developer_utils)
+modules = developer_utils.setup_addon_modules(__path__, __name__, "bpy" in locals())
+
+class LeaderAddonPreferencesData(bpy.types.PropertyGroup):
+    bl_idname = ""
+
+    label = StringProperty()
+    props = []
+
+    def draw(self, context):
+        col = self.layout.col()
+        col.label(text=self.label)
+        return
+
+class LeaderPreferencesList(UIList):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            item.draw(context, layout)
+        elif self.layout_type in {'GRID'}:
+            layout.alignment = 'CENTER'
+            layout.label(text="", icon_value=icon)
+
+class LeaderHelpersAddonPreferences(AddonPreferences):
+    bl_idname = __name__
+
+    addon_preferences_list = []
+
+    #def layer_manager_categyory_updated(self, context):
+
+    from . import layer_manager
+
+    layer_manager_category = StringProperty(default="Layers", name="Layer Manager Panel Name", update=layer_manager.update_panel)
+
+    def draw(self, context):
+        #self.layout.template_list("LeaderPreferencesList", "", self, "preference_data", self, "index")
+
+        # for drawable in LeaderHelpersAddonPreferences.addon_preferences_list:
+        #     drawfunc = getattr(drawable, "draw", None)
+        #     if callable(drawfunc):
+        #         #drawfunc(drawable, context, self.layout)
+        #         self.layout.prop
+        layout = self.layout
+        layout.prop(self, "layer_manager_category")
+        return
+# register
+##################################
+
+import traceback
+
 def register():
-    #bpy.utils.register_module(__name__)
-    UV_Helpers.register()
+    try: bpy.utils.register_module(__name__)
+    except: traceback.print_exc()
+
+    print("Registered {} with {} modules".format(bl_info["name"], len(modules)))
+    
+    for module in modules:
+        register_func = getattr(module, "register", None)
+        if callable(register_func):
+            register_func()
 
 def unregister():
-    #bpy.utils.unregister_module(__name__)
-    UV_Helpers.unregister()
+    for module in modules:
+        unregister_func = getattr(module, "unregister", None)
+        if callable(unregister_func):
+            unregister_func()
+    
+    try: bpy.utils.unregister_module(__name__)
+    except: traceback.print_exc()
+
+    print("Unregistered {}".format(bl_info["name"]))
+
+print("__init__.py running? {}".format(__name__))
 
 if __name__ == "__main__":
     register()
